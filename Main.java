e217759f0328fa784a6ee0b86307e84963b8342f
Rafael Gadimov <cowboycore@yandex.ru>
2022-12-06 23:36:19 +0300

    Second commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] m = {2, 3};
+        SalesManager sm;
+        sm = new SalesManager(m);
+
+        System.out.println(sm.max());
+    }
+}
